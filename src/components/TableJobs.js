2e79fc06edfa25156ff6be4b9d680b1ada60c416
Charity Hilton <Charity.Hilton@gtri.gatech.edu>
2018-10-28 11:52:47 -0400

    add tools for deleting and killing jobs

--- src/components/TableJobs.js
+++ src/components/TableJobs.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import { Table, Input, Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
 import Moment from 'react-moment';
 import 'moment-timezone';
-import { FaCode, FaFileAlt } from 'react-icons/fa';
+import { FaCode, FaFileAlt, FaStop, FaTrash } from 'react-icons/fa';
 import ReactJson from 'react-json-view'
+import axios from "axios";
 
 
 class TableJobs extends Component {
@@ -15,21 +16,39 @@ class TableJobs extends Component {
         this.toggle = this.toggle.bind(this);
         this.showJSON = this.showJSON.bind(this);
         this.showNLPQL = this.showNLPQL.bind(this);
-
+        this.killJob = this.killJob.bind(this);
+        this.jobToggle = this.jobToggle.bind(this);
+        this.deleteJob = this.deleteJob.bind(this);
+        this.takeSeriousAction = this.takeSeriousAction.bind(this);
+        if (props.filter !== '') {
+            props.getFilter(props.filter);
+        }
         this.state = {
-            filter: '',
             jobs: props.jobs,
             modal: false,
             modal_title: "View NLPQL",
             modal_type: "NLPQL",
             nlpql: '',
-            config: {}
+            config: {},
+            job_modal: false,
+            job_modal_title: "Kill Job",
+            job_modal_type: "KILL",
+            job_id:'-1',
+            job_danger_status: '',
+            can_continue_action: true,
+            filter: props.filter
         };
     }
 
     toggle() {
         this.setState({
             modal: !this.state.modal
+        });
+    }
+
+    jobToggle() {
+        this.setState({
+            job_modal: !this.state.job_modal
         });
     }
 
@@ -54,6 +73,49 @@ class TableJobs extends Component {
         });
     }
 
+    killJob(jobId) {
+        this.setState({
+            job_modal: true,
+            job_modal_title: "Kill job " + jobId,
+            job_modal_type: "KILL",
+            job_id: jobId,
+            job_danger_status: ''
+        });
+    }
+
+    deleteJob(jobId) {
+        this.setState({
+            job_modal: true,
+            job_modal_title: "Delete job " + jobId,
+            job_modal_type: "DELETE",
+            job_id: jobId,
+            job_danger_status: ''
+        });
+    }
+
+    takeSeriousAction() {
+        this.setState({
+            can_continue_action: false
+        });
+        let action_url = this.props.url + "delete_job/" + this.state.job_id
+        if (this.state.job_modal_type === "KILL") {
+            action_url = this.props.url + "kill_job/" + this.state.job_id
+        }
+
+        axios.get(action_url).then(response => {
+            this.setState({
+                job_danger_status: response.data,
+                job_id: -1
+            });
+            this.props.refreshJobs();
+            setTimeout(()=> {
+                this.setState({
+                    job_modal: false,
+                    can_continue_action: true
+                });
+            }, 1000)
+        });
+    }
 
     getStatus(row) {
         if (row.status !== "IN_PROGRESS") {
@@ -68,17 +130,7 @@ class TableJobs extends Component {
 
     keyUpHandler() {
         let txt = document.getElementById('jobs_filter').value.toLowerCase();
-
-        setTimeout(() => {
-            let new_jobs = this.props.jobs.filter((f) => {
-                let name = f.phenotype_name.toLowerCase();
-                return name.indexOf(txt) >= 0;
-            });
-            this.setState({
-                filter: txt,
-                jobs: new_jobs
-            });
-        }, 25);
+        this.props.getFilter(txt);
 
     };
 
@@ -88,10 +140,23 @@ class TableJobs extends Component {
                 jobs: this.props.jobs
             })
         }
+
+        if (this.props.filter !== this.state.filter) {
+            this.setState({
+                filter: this.props.filter
+            });
+            // this.props.getFilter(this.props.filter);
+        }
+    }
+
+    componentDidMount() {
+        if (this.state.filter !== '') {
+            document.getElementById('jobs_filter').value = this.state.filter;
+        }
     }
 
     render() {
-        const header_items =  ["Job ID", "Name", "Phenotype ID", "Status", "Date", "Download CSV", "View Source" ].map((h) => {
+        const header_items =  ["Job ID", "Name", "Phenotype ID", "Status", "Date", "Download CSV", "Actions"].map((h) => {
             return <th key={h}>{h}</th>;
         });
         let job_items = this.state.jobs.map((p) => {
@@ -111,6 +176,12 @@ class TableJobs extends Component {
                 <td>
                     <span title="View NLPQL" className="JobListIcons" onClick={(e) => this.showNLPQL(p, e)}><FaFileAlt /></span>
                     <span title="View JSON" onClick={(e) => this.showJSON(p, e)}><FaCode/></span>
+                    <span style={{"paddingRight": "20px"}}>&nbsp;</span>
+                    {p.status === "IN_PROGRESS" ?
+                        <span title="Kill Job" onClick={(e) => this.killJob(p.nlp_job_id)}><FaStop/></span>
+                        :
+                        <span title="Delete Job" onClick={(e) => this.deleteJob(p.nlp_job_id)}><FaTrash/></span>
+                    }
                 </td>
             </tr>;
         });
@@ -143,6 +214,16 @@ class TableJobs extends Component {
                     </ModalBody>
                     <ModalFooter>
                         <Button color="secondary" onClick={this.toggle}>Close</Button>
+                    </ModalFooter>
+                </Modal>
+                <Modal isOpen={this.state.job_modal} toggle={this.jobToggle} className="JobModal">
+                    <ModalHeader toggle={this.jobToggle}>{this.state.job_modal_title}</ModalHeader>
+                    <ModalBody>
+                        { "Are you sure you want to " + this.state.job_modal_type.toLowerCase() + " this job?"}
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="danger" onClick={this.takeSeriousAction}>Continue</Button>
+                        <Button color="secondary" onClick={this.jobToggle}>Cancel</Button>
                     </ModalFooter>
                 </Modal>
             </div>
