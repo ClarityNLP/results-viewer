40bf504a7d5a2712920f5a9b66ec1f39a9b31bd6
Caleb Sides <calebbsides@gmail.com>
2019-04-08 16:23:23 -0400

    added query by id to runner (#21)

--- src/containers/runner_container.js
+++ src/containers/runner_container.js
@@ -1,19 +1,20 @@
-import { connect } from "react-redux";
-import { setNLPQL } from "../redux/actions/set-nlpql";
-import { setNLPQLJSON } from "../redux/actions/set-nlpql-json";
-import { postToClarityAPI } from "../redux/actions/post-clarity-api";
-import { saveNLPQL } from "../redux/actions/save-nlpql";
-import JobRunner from "../components/JobRunner";
+import { connect } from 'react-redux';
+import { setNLPQL } from '../redux/actions/set-nlpql';
+import { setNLPQLJSON } from '../redux/actions/set-nlpql-json';
+import { postToClarityAPI } from '../redux/actions/post-clarity-api';
+import { saveNLPQL } from '../redux/actions/save-nlpql';
+import JobRunner from '../components/JobRunner';
 
 function mapStateToProps(state) {
-    return {
-        runner: state.runner
-    };
+  return {
+    runner: state.runner,
+    oidc: state.oidc
+  };
 }
 
 const JobRunnerContainer = connect(
-    mapStateToProps,
-    { postToClarityAPI, setNLPQL, setNLPQLJSON, saveNLPQL }
+  mapStateToProps,
+  { postToClarityAPI, setNLPQL, setNLPQLJSON, saveNLPQL }
 )(JobRunner);
 
 export default JobRunnerContainer;
